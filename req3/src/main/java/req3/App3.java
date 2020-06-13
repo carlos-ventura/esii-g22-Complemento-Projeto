c1a150a018d9f5dd5371b791c86d7b244a49e263
cdpva-iscteiul <55886769+cdpva-iscteiul@users.noreply.github.com>
2020-06-13 03:33:15 +0100

    Fixed last commit

--- req3/src/main/java/req3/App3.java
+++ req3/src/main/java/req3/App3.java
@@ -355,9 +355,9 @@ public class App3 {
 		html += "<tr>";
 		for (int j = 0; j < metadata.get(i).size(); j++) {
 			if (j == 0) {
-				s += "<td>" + "<a href=" + wordpress_url + "//" + file_names.get(i) + ">" + metadata.get(i).get(j)
+				s += "<td>" + "<a href=" + wordpress_url + "/" + file_names.get(i) + ">" + metadata.get(i).get(j)
 						+ "</a></td>";
-				html += "<td>" + "<a href=" + wordpress_url + "//" + file_names.get(i) + ">"
+				html += "<td>" + "<a href=" + wordpress_url + "/" + file_names.get(i) + ">"
 						+ metadata.get(i).get(j) + "</a></td>"; // URL to computer path
 			} else {
 				s += "<td>" + metadata.get(i).get(j) + "</td>";
