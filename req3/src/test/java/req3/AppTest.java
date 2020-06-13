a003e5d38a47bba6e517d9edf6c313119487ec90
cdpva-iscteiul <55886769+cdpva-iscteiul@users.noreply.github.com>
2020-06-13 04:50:27 +0100

    Adjustments

--- req3/src/test/java/req3/AppTest.java
+++ req3/src/test/java/req3/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest extends TestCase{
 	/**
 	 * Create the test case
 	 *
-	 * @param apptest name of the test case
+	 * @param testName name of the test case
 	 */
 	public AppTest( String apptest){
 		super( apptest );
